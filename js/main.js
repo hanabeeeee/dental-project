ab90efb364c650f7bc8acf5354d09cb851c26e43
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-08-09 15:42:55 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -47,3 +47,13 @@ $(document).ready(function(){
     
       });
 })
+
+$(function () {
+  $('header nav .depth1 li').hover(
+      function () {
+          $(this).find('.depth2').addClass('on');
+      },
+      function () {
+          $(this).find('.depth2').removeClass('on');
+      });
+});
