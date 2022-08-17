34d6943c61bc5f43120466d1cadaeab049743636
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-08-17 13:15:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 
+
+$(function(){
+
+    // //slider
+    // let num = 0;
+
+    // function changeSlider (img_num){
+    //     let img_width = 1920 * img_num;
+
+    //     $('.slider .sliders').css({transform: `translateX(${-img_width}px)`
+    //     })//slider
+    // }
+
+    // //자동 재생
+    // setInterval(function(){
+    //     num++;
+    //     if(num > 2) {num = 0;}
+    //     changeSlider(num);
+    // }, 3000)
+});
+
+
 $(document).ready(function(){
-    const swiper = new Swiper('#main-visual .swiper-container', {
+    const swiper = new Swiper('#main-visual .swiper', {
   
         autoplay: {
           delay: 3000,
@@ -50,19 +72,14 @@ $(document).ready(function(){
 
 
 
-// $(function(){
-//   $(window).scroll(function(){
-//     var scrollPosition = $(window).scrollTop();
-//     console.log(scrollPosition);
-//     if( scrollPosition >= 600 ){
-//       $('#fixed-menu').stop().slideDown();
-//     } else {
-//       $('#fixed-menu').stop().slideUp();
-//     }
-//   });
-// });
-
-window.onload = function() {
-    // AOS 초기화
-    AOS.init();
-}
+$(function(){
+  $(window).scroll(function(){
+    var scrollPosition = $(window).scrollTop();
+    console.log(scrollPosition);
+    if( scrollPosition >= 600 ){
+      $('#fixed-menu').stop().slideDown();
+    } else {
+      $('#fixed-menu').stop().slideUp();
+    }
+  });
+});
