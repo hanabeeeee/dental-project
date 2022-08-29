ecb49dafb042a1a3bd8c03a37c9cf21e768b673b
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-08-29 15:59:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-
-
 $(function(){
 
-    // //slider
-    // let num = 0;
+  // //slider
+  // let num = 0;
 
-    // function changeSlider (img_num){
-    //     let img_width = 1920 * img_num;
+  // function changeSlider (img_num){
+  //     let img_width = 1920 * img_num;
 
-    //     $('.slider .sliders').css({transform: `translateX(${-img_width}px)`
-    //     })//slider
-    // }
+  //     $('.slider .sliders').css({transform: `translateX(${-img_width}px)`
+  //     })//slider
+  // }
 
-    // //자동 재생
-    // setInterval(function(){
-    //     num++;
-    //     if(num > 2) {num = 0;}
-    //     changeSlider(num);
-    // }, 3000)
+  // //자동 재생
+  // setInterval(function(){
+  //     num++;
+  //     if(num > 2) {num = 0;}
+  //     changeSlider(num);
+  // }, 3000)
 });
 
 
@@ -25,11 +23,13 @@ $(document).ready(function(){
     const swiper = new Swiper('#main-visual .swiper', {
   
         autoplay: {
-          delay: 3000,
+          delay: 4000,
         },
         
         direction: 'horizontal',  // 효과
         loop: true,
+
+        effect: 'fade',
         
       // If we need pagination
         pagination: {
@@ -52,38 +52,41 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
-  const swiper = new Swiper('#main-visual-mobile .swiper-container', {
+const swiper = new Swiper('#main-visual-mobile .swiper-container', {
 
-      autoplay: {
-        delay: 3000,
-      },
-      
-      direction: 'horizontal',  // 효과
-      loop: true,
-      
-    // If we need pagination
-      pagination: {
-          el: '.swiper-pagination',
-      },
+    autoplay: {
+      delay: 3000,
+    },
+    
+    direction: 'horizontal',  // 효과
+    loop: true,
+    
+  // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-  
-    });
+
+  });
 })
 
 // 이미지 및 콘텐츠 로딩 체크
 window.onload = function() {
-    // AOS 초기화
-    AOS.init();
+  // AOS 초기화
+  AOS.init();
 }
 
-// $(function(){
-//   $(window).scroll(function(){
-//     var scrollPosition = $(window).scrollTop();
-//     console.log(scrollPosition);
-//     if( scrollPosition >= 600 ){
-//       $('#fixed-menu').stop().slideDown();
-//     } else {
-//       $('#fixed-menu').stop().slideUp();
-//     }
-//   });
-// });
+let vw = document.documentElement.clientWidth;
+if( vw > 768){
+$(function(){
+$(window).scroll(function(){
+  var scrollPosition = $(window).scrollTop();
+  console.log(scrollPosition);
+  if( scrollPosition >= 600 ){
+    $('#fixed-menu').stop().fadeIn();
+  } else {
+    $('#fixed-menu').stop().fadeOut();
+  }
+});
+});
+}
